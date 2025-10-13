3a2e8f2f48cdc937c1842161f368f503fd5934eb
skai273 <kinoshita116@gmail.com>
2025-10-13 19:01:17 +0900

    show physical name roughly

--- src/components/atoms/tableNode/index.tsx
+++ src/components/atoms/tableNode/index.tsx
@@ -1,7 +1,7 @@
 import { Node, NodeProps } from "reaflow";
 import { Table } from "../../../types/table";
 
-export const TableNode = (props: NodeProps) => {
+export const TableNode = (props: NodeProps<Table>) => {
   const data: Table | undefined = props.properties.data;
   if (!data) {
     return null;
@@ -13,25 +13,19 @@ export const TableNode = (props: NodeProps) => {
         fill: `rgb(${data.color.r}, ${data.color.g}, ${data.color.b})`,
         stroke: "#333",
         strokeWidth: 2,
-        rx: 12, // 角丸
       }}
     >
-      {(nodeProps) => (
-        <foreignObject width={nodeProps.width} height={nodeProps.height}>
-          <div
-            style={{
-              width: "100%",
-              height: "100%",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              fontWeight: "bold",
-            }}
-          >
-            {nodeProps.node.text}
-          </div>
-        </foreignObject>
-      )}
+      {(nodeProps) => {
+        const nodeData: Table = nodeProps.node.data;
+        return (
+          <foreignObject width={nodeProps.width} height={nodeProps.height}>
+            <div className="h-5">
+              <p className="text-3 text-center">{nodeData.physical_name}</p>
+            </div>
+            <div className="w-full h-full bg-white"></div>
+          </foreignObject>
+        );
+      }}
     </Node>
   );
 };
