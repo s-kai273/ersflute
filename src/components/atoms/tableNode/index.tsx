fb8778b448a28bc9ce5b07ecf387bf101a885822
skai273 <kinoshita116@gmail.com>
2025-10-24 01:10:38 +0900

    modify TableNode to display column info

--- src/components/atoms/tableNode/index.tsx
+++ src/components/atoms/tableNode/index.tsx
@@ -1,8 +1,8 @@
-import { NodeProps } from "@xyflow/react";
+import { Node, NodeProps } from "@xyflow/react";
 import { Table } from "../../../types/table";
+import { CheckCircleIcon, KeyIcon } from "@heroicons/react/16/solid";
 
-export function TableNode({ width, height, ...props }: NodeProps) {
-  const data = props.data as Table;
+export function TableNode({ width, height, data }: NodeProps<Node<Table>>) {
   return (
     <div
       className="flex flex-col min-h-0 rounded-sm"
@@ -16,7 +16,28 @@ export function TableNode({ width, height, ...props }: NodeProps) {
         <p className="text-sm">{data.physicalName}</p>
       </div>
       <div className="nodrag flex-1 w-full px-1 pb-1">
-        <div className="w-full h-full bg-white"></div>
+        <div className="w-full h-full bg-white">
+          {data.columns?.normalColumn.map((column) => (
+            <p className="flex items-center text-[0.625rem] leading-5 whitespace-nowrap">
+              <span className="flex items-center justify-center w-4 h-4">
+                {column.primaryKey && (
+                  <KeyIcon width={10} height={10} className="text-yellow-500" />
+                )}
+              </span>
+              <span className="flex items-center justify-center w-4 h-4">
+                {column.notNull && (
+                  <CheckCircleIcon
+                    width={10}
+                    height={10}
+                    className="text-green-400"
+                  />
+                )}
+              </span>
+              {column.physicalName && <span>{column.physicalName}</span>}
+              {column.columnType && <span>: {column.columnType}</span>}
+            </p>
+          ))}
+        </div>
       </div>
     </div>
   );
