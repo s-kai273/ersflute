baa0770cb3b5a3941721d394e9f386c8f2b0caeb
skai273 <kinoshita116@gmail.com>
2025-11-30 23:28:53 +0900

    refactor: move reactflow code in App.tsx to adapters files

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import "./App.css";
-import "@xyflow/react/dist/style.css";
-import { ReactFlowProvider } from "@xyflow/react";
 import { DbDiagram } from "@/features/dbdiagram";
 import { Toolbar } from "@/features/toolbar";
+import "./App.css";
 
 function App() {
   return (
     <div className="flex h-screen w-screen bg-slate-100">
       <Toolbar />
       <main className="flex flex-1">
-        <ReactFlowProvider>
-          <DbDiagram />
-        </ReactFlowProvider>
+        <DbDiagram />
       </main>
     </div>
   );
