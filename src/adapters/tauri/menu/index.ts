6a8b78a9bab0156cce6174da3f7e6a9a334766df
skai273 <kinoshita116@gmail.com>
2025-12-08 00:17:50 +0900

    chore: add comment for menu implementation

--- src/adapters/tauri/menu/index.ts
+++ src/adapters/tauri/menu/index.ts
@@ -1,6 +1,8 @@
 import { Menu } from "@tauri-apps/api/menu";
 import { fileMenu } from "./file";
 
+// Although Tauri provides PredefinedMenuItem, its behavior is inconsistent on Linux.
+// To ensure consistent cross-platform behavior, an equivalent menu item is implemented manually.
 export async function setupWindowMenu() {
   const menu = await Menu.new({
     items: [fileMenu],
