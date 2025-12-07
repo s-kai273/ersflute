c306204a49aec3d9a0da5bb9792f13c87effb443
skai273 <kinoshita116@gmail.com>
2025-12-07 23:54:31 +0900

    chore: install tauri-plugin-os

--- src-tauri/app/src/lib.rs
+++ src-tauri/app/src/lib.rs
@@ -11,6 +11,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_os::init())
         .invoke_handler(tauri::generate_handler![load_diagram])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
